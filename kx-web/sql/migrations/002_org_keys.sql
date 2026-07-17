0d51a231f3b43d819d92f8c348be261067b247f9
proalvo <k_nykanen@hotmail.com>
2026-07-17 21:34:12 +0300

    Add org_key_hash and org_key_hint to organization table
    Added columns for organization provisioning keys.

--- kx-web/sql/migrations/002_org_keys.sql
+++ kx-web/sql/migrations/002_org_keys.sql
@@ -1,1 +1,5 @@
-
+-- Organization provisioning keys: allow kx-server to create competitions
+ALTER TABLE organization
+  ADD COLUMN org_key_hash VARCHAR(255) NOT NULL DEFAULT '' AFTER status,
+  ADD COLUMN org_key_hint CHAR(4)      NOT NULL DEFAULT '' AFTER org_key_hash;
+INSERT IGNORE INTO schema_migration (version) VALUES (2);
